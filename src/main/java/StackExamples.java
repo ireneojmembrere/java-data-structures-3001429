f37dd855a4e74f1290e6cc8b3887efe7ed18d0f1
Ireneo James Membrere <40286207+ireneojmembrere@users.noreply.github.com>
2024-08-21 18:20:06 +0000

    stack example

--- src/main/java/StackExamples.java
+++ src/main/java/StackExamples.java
@@ -3,6 +3,13 @@ import java.util.Deque;
 
 public class StackExamples {
 
+  // Return the same Stack as given in the input, but with the top number replaced by newTop.
+  public static Deque<Integer> updateStack(Deque<Integer> stack, int newTop) {
+    stack.pop();
+    stack.push(newTop);
+    return stack;
+}
+
   public static void main(String[] args) {
     
     Deque<String> stack = new ArrayDeque<>();
@@ -16,6 +23,17 @@ public class StackExamples {
     stack.poll();
     System.out.println(stack);
 
+
+    // testing updateStack
+    Deque<Integer> stack2 = new ArrayDeque<>();
+    stack2.push(2);
+    stack2.push(3);
+    stack2.push(5);
+    stack2.push(7);
+    stack2.push(10);
+    int newTop = 11;
+    Deque<Integer> result = updateStack(stack2, newTop); 
+    System.out.println(result); // expected [11, 7, 5, 3, 2]
   }
 
 }
