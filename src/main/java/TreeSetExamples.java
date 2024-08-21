b3f7f89021a0af6dfb632415afb7f9c0ca58bc32
Ireneo James Membrere <40286207+ireneojmembrere@users.noreply.github.com>
2024-08-21 19:04:01 +0000

    treeset example

--- src/main/java/TreeSetExamples.java
+++ src/main/java/TreeSetExamples.java
@@ -4,6 +4,16 @@ import java.util.TreeSet;
 
 public class TreeSetExamples {
 
+  // Return a TreeSet containing the three words, ordered by length.
+  public static TreeSet<String> createSortedTreeSet(String word1, String word2, String word3) {
+    // Your code goes here.
+    TreeSet<String> treeSet = new TreeSet<>(Comparator.comparing(String::length));
+    treeSet.add(word1);
+    treeSet.add(word2);
+    treeSet.add(word3);
+    return treeSet;
+}
+
   public static void main(String[] args) {
 
     Set<Integer> treeSet = new TreeSet<>();
@@ -25,6 +35,13 @@ public class TreeSetExamples {
     wordSet.remove("giraffe");
     System.out.println(wordSet);
 
+    // testing createSortedTreeSet
+    TreeSet<String> result1 = createSortedTreeSet("red","yellow","blue");
+    System.out.println(result1);
+    TreeSet<String> result2 = createSortedTreeSet("car","bicycle","truck");
+    System.out.println(result2);
+    TreeSet<String> result3 = createSortedTreeSet("pineapple","pear","papaya");
+    System.out.println(result3);
   }
 
 }
